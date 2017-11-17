6fd4301f0ef3b8fcbed384876d4a7fdeaa623422
unknown <lubomir.lackovic@gmail.com>
2017-11-17 16:50:51 +0100

    initial commit

---
+++ hello.py
@@ -0,0 +1,8 @@
+x = int(input("Sirka: "));
+y = int(input("Dlzka: "));
+z = int(input("Vyska: "));
+lm2 = int(input ("Litrov farby na m2: "));
+
+print ("Treba tolkoto litrov farby: " + (2*x*z+2*y*z+x*y)*lm2);
+
+###test
---
+++ test.py
@@ -0,0 +1,9 @@
+
+i = 0
+
+while i < 5:
+    print(i)
+    i+=1
+print ("new i:" + str(i))
+
+###vvv
